1c33e99dfd8cb45c181bc99015f576b69c305707
IqbalAli1104 <iqbalbasyair19@gmail.com>
2023-05-26 21:19:07 +0700

    add revision

--- src/Redux/todoreducer.js
+++ src/Redux/todoreducer.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const todoReducer = createSlice({
     name : "todos",
     initialState : [
-
+        {id : 1, title: "todo1", completed : false},
     ],
     reducers: {
         addTodo: (state,action) => {
